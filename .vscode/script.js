d2c3c31e6585ce75918288ecd4ee7e88d233a9dc
SMRebelo <137157267+SMRebelo@users.noreply.github.com>
2023-09-27 00:17:46 +0100

    first commit

---
+++ .vscode/script.js
@@ -0,0 +1,20 @@
+function toggleMode(){
+  const html = document.documentElement // pega o html
+  html.classList.toggle("light")
+
+  // tenho que pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode adicionar imagem light
+    img.setAttribute('src', './assets/Avatar.png');// ajustar , modificar um atributo!
+    img.setAttribute('alt', 'Foto de perfil durante o dia')
+  } else {
+    //se tiver dark mode adicionar imagem normal
+    img.setAttribute("src", "./assets/avatar-dark.png");
+    img.setAttribute("alt", "Foto de perfil durante o Surf")
+  }
+
+  
+
+}
